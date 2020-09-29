df1cc4936e0ce2a55a93089517fe724784a5bc00
Justin Kchao <Justin.Kchao@gmail.com>
2020-09-28 20:12:19 -0700

    Updates

---
+++ precache-manifest.35f11610b0856b4b0b67f1a826a66564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc0083da6e00e15e7fd4ab9fbcd4d7fc",
+    "url": "/chemapp/index.html"
+  },
+  {
+    "revision": "d7fa67d3c604489c461f",
+    "url": "/chemapp/static/css/main.642f5899.chunk.css"
+  },
+  {
+    "revision": "0729014791bd224712ae",
+    "url": "/chemapp/static/js/2.41221713.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/chemapp/static/js/2.41221713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7fa67d3c604489c461f",
+    "url": "/chemapp/static/js/main.59628a05.chunk.js"
+  },
+  {
+    "revision": "66c22a3415e87f4aa61c",
+    "url": "/chemapp/static/js/runtime-main.c3628091.js"
+  }
+]);
